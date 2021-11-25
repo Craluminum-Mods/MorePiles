e55a224cd8ccc574f4156263049fc408c26084a1
Craluminum2413 <beware.14b@gmail.com>
2021-11-25 17:08:18 +0200

    Updated to 1.1.1

--- src/MP.cs
+++ src/MP.cs
@@ -1,4 +1,4 @@
-using Vintagestory.API.Common;
+﻿using Vintagestory.API.Common;
 
 [assembly: ModInfo( "MorePiles",
 	Description = "Adds piles for some items and blocks",
@@ -28,6 +28,14 @@ namespace MorePiles
 			}
 
 			api.World.Config.SetBool("MPangledgear", MorePilesConfig.Loaded.PileAngledGear);
+			api.World.Config.SetBool("MParrow", MorePilesConfig.Loaded.PileArrow);
+			// api.World.Config.SetBool("MParrow128", MorePilesConfig.Loaded.PileArrowStackingCapacity128);
+			// api.World.Config.SetBool("MParrow112", MorePilesConfig.Loaded.PileArrowStackingCapacity112);
+			// api.World.Config.SetBool("MParrow96", MorePilesConfig.Loaded.PileArrowStackingCapacity96);
+			// api.World.Config.SetBool("MParrow80", MorePilesConfig.Loaded.PileArrowStackingCapacity80);
+			// api.World.Config.SetBool("MParrow64", MorePilesConfig.Loaded.PileArrowStackingCapacity64);
+			// api.World.Config.SetBool("MParrow48", MorePilesConfig.Loaded.PileArrowStackingCapacity48);
+			// api.World.Config.SetBool("MParrow32", MorePilesConfig.Loaded.PileArrowStackingCapacity32);
 			api.World.Config.SetBool("MPaxle", MorePilesConfig.Loaded.PileAxle);
 			api.World.Config.SetBool("MPbamboostakes", MorePilesConfig.Loaded.PileBambooStakes);
 			api.World.Config.SetBool("MPbeeswax", MorePilesConfig.Loaded.PileBeeswax);
@@ -42,12 +50,16 @@ namespace MorePiles
 			api.World.Config.SetBool("MPflaxtwine", MorePilesConfig.Loaded.PileFlaxTwine);
 			api.World.Config.SetBool("MPflowerpotburnt", MorePilesConfig.Loaded.PileFlowepotBurnt);
 			api.World.Config.SetBool("MPhenbox", MorePilesConfig.Loaded.PileHenbox);
+			api.World.Config.SetBool("MPhoneycomb", MorePilesConfig.Loaded.PileHoneycomb);
+			api.World.Config.SetBool("MPironbloom", MorePilesConfig.Loaded.PileIronBloom);
 			api.World.Config.SetBool("MPironfence", MorePilesConfig.Loaded.PileIronFence);
 			api.World.Config.SetBool("MPladder", MorePilesConfig.Loaded.PileLadder);
 			api.World.Config.SetBool("MPmetalchain", MorePilesConfig.Loaded.PileMetalChain);
 			api.World.Config.SetBool("MPmetallamellae", MorePilesConfig.Loaded.PileMetalLamellae);
 			api.World.Config.SetBool("MPmetalscales", MorePilesConfig.Loaded.PileMetalScales);
 			api.World.Config.SetBool("MPpainting", MorePilesConfig.Loaded.PilePainting);
+			api.World.Config.SetBool("MPpoultice", MorePilesConfig.Loaded.PilePoultice);
+			api.World.Config.SetBool("MPsail", MorePilesConfig.Loaded.PileSail);
 			api.World.Config.SetBool("MPsign", MorePilesConfig.Loaded.PileSign);
 			api.World.Config.SetBool("MPstick", MorePilesConfig.Loaded.PileStick);
 			api.World.Config.SetBool("MPstickslayer", MorePilesConfig.Loaded.PileSticksLayer);
@@ -63,6 +75,14 @@ namespace MorePiles
 		{
 			public static MorePilesConfig Loaded { get; set; } = new MorePilesConfig();
 			public bool PileAngledGear { get; set; } = true;
+			public bool PileArrow { get; set; } = true;
+			// public bool PileArrowStackingCapacity128 { get; set; } = true;
+			// public bool PileArrowStackingCapacity112 { get; set; } = false;
+			// public bool PileArrowStackingCapacity96 { get; set; } = false;
+			// public bool PileArrowStackingCapacity80 { get; set; } = false;
+			// public bool PileArrowStackingCapacity64 { get; set; } = false;
+			// public bool PileArrowStackingCapacity48 { get; set; } = false;
+			// public bool PileArrowStackingCapacity32 { get; set; } = false;
 			public bool PileAxle { get; set; } = true;
 			public bool PileBambooStakes { get; set; } = true;
 			public bool PileBeeswax { get; set; } = true;
@@ -77,12 +97,16 @@ namespace MorePiles
 			public bool PileFlaxTwine { get; set; } = true;
 			public bool PileFlowepotBurnt { get; set; } = false;
 			public bool PileHenbox { get; set; } = true;
+			public bool PileHoneycomb { get; set; } = true;
+			public bool PileIronBloom { get; set; } = true;
 			public bool PileIronFence { get; set; } = true;
 			public bool PileLadder { get; set; } = true;
 			public bool PileMetalChain { get; set; } = true;
 			public bool PileMetalLamellae { get; set; } = true;
 			public bool PileMetalScales { get; set; } = true;
 			public bool PilePainting { get; set; } = true;
+			public bool PilePoultice { get; set; } = false;
+			public bool PileSail { get; set; } = true;
 			public bool PileSign { get; set; } = true;
 			public bool PileStick { get; set; } = true;
 			public bool PileSticksLayer { get; set; } = true;
